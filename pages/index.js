6cb6ad9e2a286371b1aaa499fee31775b9b396ae
milesmaroney <miles@milesmaroney.com>
2021-12-12 15:38:31 -0800

    Start of player implementation

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Sidebar from "../components/Sidebar"
 import Center from "../components/Center"
+import Player from "../components/Player"
 import { getSession } from "next-auth/react"
 
 export default function Home() {
@@ -10,8 +11,8 @@ export default function Home() {
         <Center />
       </main>
 
-      <div>
-        {/* Player */}
+      <div className="sticky bottom-0">
+        <Player />
       </div>
     </div>
   )
