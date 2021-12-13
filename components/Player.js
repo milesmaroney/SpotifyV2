8aadab66b84ef027f12acd44b35f2d33dc650e93
milesmaroney <miles@milesmaroney.com>
2021-12-12 16:24:16 -0800

    Working Volume Functionality

--- components/Player.js
+++ components/Player.js
@@ -1,6 +1,8 @@
-import { FastForwardIcon, PauseIcon, PlayIcon, ReplyIcon, RewindIcon, SwitchHorizontalIcon } from "@heroicons/react/solid";
+import { ViewListIcon, VolumeUpIcon as VolumeDownIcon } from "@heroicons/react/outline";
+import { FastForwardIcon, PauseIcon, PlayIcon, RefreshIcon, ReplyIcon, RewindIcon, SwitchHorizontalIcon, VolumeUpIcon } from "@heroicons/react/solid";
+import { debounce } from "lodash";
 import { useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
 import useSongInfo from "../hooks/useSongInfo";
@@ -37,12 +39,26 @@ function Player() {
       })
   }
 
+
   useEffect(() => {
     if (spotifyApi.getAccessToken() && !currentTrackId) {
       fetchCurrentSong();
       setVolume(50);
     }
   }, [currentTrackId, spotifyApi, session])
+
+
+  useEffect(() => {
+    if (volume > 0 && volume < 100) {
+      debouncedAdjustVolume(volume);
+    }
+  }, [volume])
+
+  const debouncedAdjustVolume = useCallback(
+    debounce(volume => {
+      spotifyApi.setVolume(volume).catch(err => {})
+    }, 500), []
+  )
 
   return (
     <div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8">
@@ -56,7 +72,7 @@ function Player() {
       </div>
 
       <div className="flex items-center justify-evenly">
-        <SwitchHorizontalIcon className="button" />
+        <ViewListIcon className="button" />
         <RewindIcon className="button" /> {/* onClick={() => spotifyApi.skipToPrevious() */}
 
         {isPlaying ? (
@@ -66,7 +82,18 @@ function Player() {
         )}
 
         <FastForwardIcon className="button" /> {/* onClick={() => spotifyApi.skipToNext() */}
-        <ReplyIcon className="button" />
+        <RefreshIcon className="button" />
+      </div>
+
+      <div className="flex items-center space-x-3 md:space-x-4 justify-end pr-5">
+        <VolumeDownIcon className="button"
+        onClick={() => volume > 0 && setVolume(volume - 10)}/>
+
+        <input className="w-15 md:w-30" type="range" value ={volume} min={0} max={100}
+        onChange={e => setVolume(Number(e.target.value))}/>
+
+        <VolumeUpIcon className="button"
+        onClick={() => volume < 100 && setVolume(volume + 10)}/>
       </div>
     </div>
   )
